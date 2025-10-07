57cc2055ed841e333cd839d248b652eba9bdb245
ManuGolem <manuelcarreras2013@gmail.com>
2025-10-06 22:58:39 -0300

    update styles

---
+++ src/Components/GraphPage.jsx
@@ -0,0 +1,55 @@
+import { Buttons } from "./Buttons";
+import { useState, useRef } from "react";
+import { useCytoscape } from "../CustomHooks/useCytoscape";
+export function GraphPage() {
+    const [tree, setTree] = useState();
+    const cyRef = useRef(null);
+    const cyTreeRef = useRef(null);
+    const graphExample = [
+        {
+            // node a
+            data: { id: "a" },
+        },
+        {
+            // node b
+            data: { id: "b" },
+        },
+        {
+            data: { id: "c" },
+        },
+        {
+            data: { id: "d" },
+        },
+        {
+            data: { id: "ab", source: "a", target: "b" },
+        },
+        {
+            data: { id: "ac", source: "c", target: "a" },
+        },
+        {
+            data: { id: "cb", source: "c", target: "b" },
+        },
+        {
+            data: { id: "bd", source: "b", target: "d" },
+        },
+    ];
+    useCytoscape(cyRef, graphExample);
+    useCytoscape(cyTreeRef, tree);
+    return (
+        <main className="graphPage">
+            <Buttons setTree={setTree} graph={graphExample} />
+            <div className="grafos">
+                <article className="container">
+                    <h1>Graph</h1>
+                    <div id="cy" ref={cyRef} className="graph" />
+                </article>
+                {tree && (
+                    <article className="container">
+                        <h1>Tree of graph</h1>
+                        <div id="cy-tree" ref={cyTreeRef} className="graph " />
+                    </article>
+                )}
+            </div>
+        </main>
+    );
+}
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,55 +1,9 @@
-import { Buttons } from "./Buttons";
-import { useState, useRef } from "react";
-import { useCytoscape } from "../CustomHooks/useCytoscape";
 export function Main() {
-    const [tree, setTree] = useState();
-    const cyRef = useRef(null);
-    const cyTreeRef = useRef(null);
-    const graphExample = [
-        {
-            // node a
-            data: { id: "a" },
-        },
-        {
-            // node b
-            data: { id: "b" },
-        },
-        {
-            data: { id: "c" },
-        },
-        {
-            data: { id: "d" },
-        },
-        {
-            data: { id: "ab", source: "a", target: "b" },
-        },
-        {
-            data: { id: "ac", source: "c", target: "a" },
-        },
-        {
-            data: { id: "cb", source: "c", target: "b" },
-        },
-        {
-            data: { id: "bd", source: "b", target: "d" },
-        },
-    ];
-    useCytoscape(cyRef, graphExample);
-    useCytoscape(cyTreeRef, tree);
     return (
         <main className="mainPage">
-            <Buttons setTree={setTree} graph={graphExample} />
-            <div className="grafos">
-                <article className="container">
-                    <h1>Graph</h1>
-                    <div id="cy" ref={cyRef} className="graph" />
-                </article>
-                {tree && (
-                    <article className="container">
-                        <h1>Tree of graph</h1>
-                        <div id="cy-tree" ref={cyTreeRef} className="graph " />
-                    </article>
-                )}
-            </div>
+            <h1>
+                Visualize algorithms in real time, watch how <span>sorting</span> and <span>graph</span> algorithms work, step by step.
+            </h1>
         </main>
     );
 }
